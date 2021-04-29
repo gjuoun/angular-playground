b34eae98af16b1284add66fdeacb876a42e92aff
John Guo <gjuoun@gmail.com>
2021-04-29 00:40:31 -0300

    finish tut

--- my-app/src/app/hero-detail/hero-detail.component.ts
+++ my-app/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../hero'
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() hero?: Hero
+
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero()
+  }
+
+  getHero() {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero!).subscribe(() => {
+      this.location.back()
+
+    })
+
   }
 
 }
--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,15 +13,28 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = HEROES;
   selectedHero?: Hero;
 
-  constructor() { }
-
-  onSelect(hero: Hero){
-    this.selectedHero = hero;
-    console.log(this.selectedHero);
-    
+  ngOnInit() {
+    this.getHeroes();
   }
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
 }
